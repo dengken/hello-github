ff20785614fb6f25770efa8baba20fc030ead59f
dengken <dengpengdexin@gmail.com>
2018-12-24 15:46:41 +0800

    多语言

--- app/src/main/java/com/juefeng/game/hellogithub/MainActivity.java
+++ app/src/main/java/com/juefeng/game/hellogithub/MainActivity.java
@@ -1,13 +1,24 @@
 package com.juefeng.game.hellogithub;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseAc {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("main", "onCreate: !!!!!");
+
+        setTitle("第一个Activity");
+        findViewById(R.id.btn_setting).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SettingAc.class));
+            }
+        });
     }
 }
